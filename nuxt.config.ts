8fcd7b1577b75174233d40e816365d26805dd1f2
Svene <hello.svene@gmail.com>
2024-03-08 10:23:20 +0800

    Add tailwindcss and shadcn modules, update package dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config',
+    exposeConfig: true,
+    config: {},
+    injectPosition: 'first',
+    viewer: true,
+  },
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  }
 })
